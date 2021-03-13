3790309b43e01e5c2c61dc5194b7dcc3736ab264
HeavyMonster911 <72610595+HeavyMonster911@users.noreply.github.com>
2021-03-13 16:42:06 +0200

    Create js.js

---
+++ js.js
@@ -0,0 +1,51 @@
+function getPasswordENG(){
+
+    var chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+><:{}[]";
+    var passwordLength = 16;
+    var password = "";
+
+    for (var i=0; i<passwordLength; i++){
+        var randomNumber = Math.floor(Math.random() *  chars.length);
+        password += chars.substring(randomNumber,randomNumber+1);
+    }
+    document.getElementById("generatorENG").value = password
+}
+
+function getPasswordRUS(){
+
+    var chars = "0123456789йцукенгшщзхъфывапролджэячсмитьбюЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ!@#$%^&*()_+><:{}[]";
+    var passwordLength = 16;
+    var password = "";
+
+    for (var i=0; i<passwordLength; i++){
+        var randomNumber = Math.floor(Math.random() *  chars.length);
+        password += chars.substring(randomNumber,randomNumber+1);
+    }
+    document.getElementById("generatorRUS").value = password
+}
+
+function getPasswordESP(){
+
+    var chars = "0123456789qwertyuiopasdfghjklñzxcvbnmQWERTYUIOPASDFGHJKLÑZXCVBNMºª·/=?¡¿çÇÑ!@#$%^&*()_+><:{}[]";
+    var passwordLength = 16;
+    var password = "";
+
+    for (var i=0; i<passwordLength; i++){
+        var randomNumber = Math.floor(Math.random() *  chars.length);
+        password += chars.substring(randomNumber,randomNumber+1);
+    }
+    document.getElementById("generatorESP").value = password
+}
+
+function getPasswordUKR(){
+
+    var chars = "0123456789йцукенгшщзхїфівапролджєячсмитьбюЙЦУКЕНГШЩЗХЇФІВАПРОЛДЖЄЯЧСМИТЬБЮ!@#$%^&*()_+><:{}[]";
+    var passwordLength = 16;
+    var password = "";
+
+    for (var i=0; i<passwordLength; i++){
+        var randomNumber = Math.floor(Math.random() *  chars.length);
+        password += chars.substring(randomNumber,randomNumber+1);
+    }
+    document.getElementById("generatorUKR").value = password
+}
